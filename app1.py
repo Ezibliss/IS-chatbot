21e119cfd3f7c6f6fac80ec86fbae7e1bcde2850
Ezibliss <118067640+Ezibliss@users.noreply.github.com>
2025-06-05 20:18:30 +0100

    app1.py

--- app1.py
+++ app1.py
@@ -19,7 +19,7 @@ documents = pdf_loader.load()
 text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 50)
 chunks = text_splitter.split_documents(documents)
 
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={"device": "cpu"})
 db = FAISS.from_documents(documents = chunks, embedding = embeddings)
 print("FAISS vector store created!")
 
